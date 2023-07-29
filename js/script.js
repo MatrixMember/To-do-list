77eabd4d133f4654f69b375d729aac62dbc473ec
Aleksander Sarna <oleksarna@gmail.com>
2023-07-29 20:24:04 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -12,12 +12,26 @@
       },
    ];
 
+   const addNewTask = (newTaskContent) => {
+      tasks.push({
+         content: newTaskContent,
+      });
+      render();
+   };
+
+   const removeTask = (taskIndex) => {
+      tasks.splice(taskIndex, 1);
+            render();
+   }
+
    const render = () => {
       let htmlString = "";
 
       for (const task of tasks) {
          htmlString += `
          <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+
+         <button class="js-remove">usuń</button>
          ${task.content}
          </li>
          `;
@@ -25,14 +39,16 @@
 
       document.querySelector(".js-tasks").innerHTML = htmlString;
 
+      const removeButtons = document.querySelectorAll(".js-remove")
+
+      removeButtons.forEach((removeButton, index) => {
+         removeButton.addEventListener("click", () => {
+            removeTask(index);
+         });
+      });
    };
 
-   const addNewTask = (newTaskContent) => {
-      tasks.push({
-         content: newTaskContent,
-      });
-      render();
-   };
+   
 
    const onFormSubmit = (event) => {
       event.preventDefault();
