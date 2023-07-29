2c3312bf89bc81204f3931c0f0fe0f2c1b08a700
Aleksander Sarna <oleksarna@gmail.com>
2023-07-29 19:12:35 +0200

    adding first functions

---
+++ js/script.js
@@ -0,0 +1,26 @@
+{
+   const tasks = [
+      {
+
+      },
+
+      {
+
+      },
+   ];
+
+   const render = () => {
+      let htmlString = "";
+
+      for(const task of tasks) {
+         htmlString += `
+         
+         `;
+      }
+   };
+
+   const init = () => {
+
+   };
+   init();
+}
