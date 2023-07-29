8a2a60a4b75dbc8d28dd327589e2496e1a2e544f
Aleksander Sarna <oleksarna@gmail.com>
2023-07-29 19:54:53 +0200

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -1,26 +1,58 @@
 {
    const tasks = [
       {
+         content: " zrobić zakupy",
+         done: false,
 
       },
 
       {
-
+         content: " pójść na trennig",
+         done: true,
       },
    ];
 
    const render = () => {
       let htmlString = "";
 
-      for(const task of tasks) {
+      for (const task of tasks) {
          htmlString += `
-         
+         <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+         ${task.content}
+         </li>
          `;
       }
+
+      document.querySelector(".js-tasks").innerHTML = htmlString;
+
    };
 
-   const init = () => {
+   const addNewTask = (newTaskContent) => {
+      tasks.push({
+         content: newTaskContent,
+      });
+      render();
+   };
 
+   const onFormSubmit = (event) => {
+      event.preventDefault();
+
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+      if (newTaskContent === "") {
+         return;
+      }
+
+      addNewTask(newTaskContent);
+
+   }
+
+   const init = () => {
+      render();
+
+      const form = document.querySelector(".js-form");
+
+      form.addEventListener("submit", onFormSubmit);
    };
    init();
 }
